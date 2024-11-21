fbf00b6adc503be256fa7a855aaba1c1015126c5
Kayla Harris <103599117+k281harri@users.noreply.github.com>
2024-11-21 08:40:27 -0600

    Add files via upload

---
+++ kba/appNOTES.js
@@ -0,0 +1,27 @@
+/* basic requirements 
+............................................
+0-3 pages
+                (html)
+0- 10 HTML logs
+0-1 table
+0-2 forms
+0-1 dropdown menu
+0-1 of each
+        0-text
+        0-image
+        -gif
+                     (css)
+0-inline
+0-internal
+0-external
+0-5 different css selectors
+
+maybe required
+=-flexbox
+-=
+=-2 css animATIONS
+=-ZIPFILE
+
+    ##############-upload 2 github
+    DUE WED 5pm<--------------------------------------
+    -------------------------------------------------------------------
